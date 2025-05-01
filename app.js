efe05322a9795d8f1e76a79ae3896cbd3d5144ce
Erin Slater <airslater93@yahoo.com>
2025-04-30 20:14:47 -0500

    adding my app

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from EC2!');
+});
+
+app.listen(80, '0.0.0.0', () => {
+    console.log('Server is running');
+  });
+  
